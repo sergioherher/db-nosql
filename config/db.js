76cd3e7851c75937404da28d956f1e5581b45938
sergioherher <sergioh81@gmail.com>
2018-11-22 22:42:31 -0430

    Se modifico la conexion a la base de datos mongodb

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbURI =
-  "mongodb://newuser:myNewPassword@cluster0-shard-00-00-gibou.mongodb.net:27017,cluster0-shard-00-01-gibou.mongodb.net:27017,cluster0-shard-00-02-gibou.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+  "mongodb://sergioherher:programar-15@cluster0-shard-00-00-lvyut.mongodb.net:27017,cluster0-shard-00-01-lvyut.mongodb.net:27017,cluster0-shard-00-02-lvyut.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 
 const options = {
   reconnectTries: Number.MAX_VALUE,
