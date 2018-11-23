7584194fd03738b58960c93dfa7c45856ef4dfeb
sergioh81@gmail.com <sergioh81@gmail.com>
2018-11-23 09:56:14 -0400

    Se modifico la conexion

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbURI =
-  "mongodb://newuser:myNewPassword@cluster0-shard-00-00-gibou.mongodb.net:27017,cluster0-shard-00-01-gibou.mongodb.net:27017,cluster0-shard-00-02-gibou.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+  "mongodb+srv://sergioherher:<PASSWORD>@cluster0-lvyut.mongodb.net/test?retryWrites=true";
 
 const options = {
   reconnectTries: Number.MAX_VALUE,
